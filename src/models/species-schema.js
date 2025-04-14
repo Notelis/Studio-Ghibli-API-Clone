360e5efdf89dbca9621187209c9ae7ca388d1d84
hannahlrhl <hannahlarissahalim@gmail.com>
2025-04-14 10:15:19 +0700

    Add uuid library to species-schema.js

--- src/models/species-schema.js
+++ src/models/species-schema.js
@@ -1,14 +1,22 @@
+const { v4: uuidv4 } = require('uuid');
+
 module.exports = (db) =>
-    db.model(
-      'Species',
-      db.Schema({
-        name: String,
-        classification: String,
-        eye_colors: String,
-        hair_colors: String,
-        people: [String],
-        films: [String],
-        url: String,
-      })
-    );
-  
+  db.model(
+    'Species',
+    db.Schema({
+      id: { 
+        type: String, 
+        default: uuidv4, 
+        unique: true 
+      },
+      name: String,
+      classification: String,
+      eye_colors: String,
+      hair_colors: String,
+      people: [String],
+      films: [String],
+      url: { 
+        type: String, 
+        unique: true },
+    })
+  );
