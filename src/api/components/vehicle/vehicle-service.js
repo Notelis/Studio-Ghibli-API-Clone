0d4d9f7720e7650a207a34adc4714b723e7cc8be
WinnyJazz <jewinn2244@gmail.com>
2025-04-16 09:55:29 +0700

    new update

--- src/api/components/vehicle/vehicle-service.js
+++ src/api/components/vehicle/vehicle-service.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const vehiclesRepository = require('./vehicles-repository');
 
 async function getVehicles(offset = 0, limit = 50) {
@@ -10,10 +11,32 @@ async function getVehicleById(id) {
 
 async function createVehicle(data) {
   return vehiclesRepository.createVehicle(data);
+=======
+const vehicleRepository = require('./vehicle-repository');
+
+// Mendapatkan semua data kendaraan
+async function getVehicles() {
+  return vehicleRepository.getVehicles();
+}
+
+// Mendapatkan kendaraan berdasarkan ID
+async function getVehicle(id) {
+  return vehicleRepository.getVehicle(id);
+}
+
+// Opsional: Mendapatkan kendaraan berdasarkan nama, jika kamu punya fitur pencarian by name
+async function getVehicleByName(name) {
+  return vehicleRepository.getVehicleByName({ name });
+>>>>>>> Stashed changes
 }
 
 module.exports = {
   getVehicles,
+<<<<<<< Updated upstream
   getVehicleById,
   createVehicle, 
+=======
+  getVehicle,
+  getVehicleByName, // opsional, bisa dihapus jika tidak diperlukan
+>>>>>>> Stashed changes
 };
