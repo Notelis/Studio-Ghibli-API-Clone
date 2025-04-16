19b2b17d8b9aaec478a985a143d8dde9ecb7f4e9
hannahlrhl <hannahlarissahalim@gmail.com>
2025-04-16 09:31:16 +0700

    add species-repository.js

---
+++ src/api/components/species/species-repository.js
@@ -0,0 +1,21 @@
+const { Species } = require('../../../models');
+
+// Get all species
+
+async function getSpecies() {
+  return Species.find({});
+}
+
+// Get a species by ID
+
+async function getSpeciesById(id) {
+  return Species.findById(id);
+}
+
+module.exports = {
+  getSpecies,
+  getSpeciesById,
+  createSpecies,
+  updateSpecies,
+  deleteSpecies,
+};
