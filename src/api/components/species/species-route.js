a6304a6bc92b12c1b708a02fd7d499ffa9cdce89
hannahlrhl <hannahlarissahalim@gmail.com>
2025-04-22 19:46:00 +0700

    fix some typo

--- src/api/components/species/species-route.js
+++ src/api/components/species/species-route.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const speciesController = require('./species-controller');
 
-const authMiddleware = require('../../../../utils/authenticate-token');
+const { authenticateToken } = require('../../../utils/authenticate-token');
 
 const route = express.Router();
  
@@ -10,10 +10,10 @@ module.exports = (app) => {
     app.use('/species', route);
  
     // Get all species (but only if authenticated)
-    route.get('/', authMiddleware, speciesController.getSpecies);
+    route.get('/', authenticateToken, speciesController.getSpecies);
 
     // Get species by ID (also only if authenticated)
-    route.get('/:id', authMiddleware, speciesController.getSpeciesById);
+    route.get('/:id', authenticateToken, speciesController.getSpeciesById);
 
    // Get all species
    //route.get('/', speciesController.getSpecies);
