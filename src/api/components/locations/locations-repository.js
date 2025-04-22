b6de9e954882c29f96b2128f8c44f030a61aa1f8
KarelFelixJuanito <karelfelix14@gmail.com>
2025-04-22 15:31:04 +0700

    update all locations

--- src/api/components/locations/locations-repository.js
+++ src/api/components/locations/locations-repository.js
@@ -1,19 +1,26 @@
 const { locations } = require('../../../models');
 
-async function getLocations() {
-    return locations.find({});
-}
+async function getLocations(filter = {}, limit = 0, offset = 0) {
+    const query = {};
+  
+    if (filter.name) {
+      query.name = new RegExp(filter.name, 'i');
+    }
+
+    if (filter.terrain) {
+        query.terrain = new RegExp(filter.terrain, 'i');
+      }
+  
+    return People.find(query)
+      .skip(offset)
+      .limit(limit);
+  }
 
 async function getLocationById(id){
     return locations.findById(id);
 }
 
-async function getLocationByTerrain(terrain){
-    return locations.findByTerrain({ terrain });
-}
-
 module.exports = {
     getLocations,
     getLocationById,
-    getLocationByTerrain,
 }
