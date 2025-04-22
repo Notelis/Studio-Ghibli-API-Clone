42bd72fdfeedaca1ace0753cddf068aacc306a7d
hannahlrhl <hannahlarissahalim@gmail.com>
2025-04-22 18:19:46 +0700

    add authentication to species-route.js

--- src/api/components/species/species-route.js
+++ src/api/components/species/species-route.js
@@ -2,15 +2,26 @@ const express = require('express');
 
 const speciesController = require('./species-controller');
 
+const authMiddleware = require('../../../../utils/authentication-token');
+
 const route = express.Router();
  
 module.exports = (app) => {
     app.use('/species', route);
  
+    // Get all species (but only if authenticated)
+    route.get('/', authMiddleware, speciesController.getSpecies);
+
+    // Get species by ID (also only if authenticated)
+    route.get('/:id', authMiddleware, speciesController.getSpeciesById);
+
    // Get all species
-   route.get('/', speciesController.getSpecies);
- 
+   //route.get('/', speciesController.getSpecies);
+   
    // Get species by ID
-   route.get('/:id', speciesController.getSpeciesById);
+   //route.get('/:id', speciesController.getSpeciesById);
  };
+
+ 
+
  
