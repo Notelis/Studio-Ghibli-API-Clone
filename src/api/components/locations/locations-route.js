871782a6fe87bbad3130f889bf60cc3323a6e330
KarelFelixJuanito <karelfelix14@gmail.com>
2025-04-23 07:50:46 +0700

    Update locations-route.js

--- src/api/components/locations/locations-route.js
+++ src/api/components/locations/locations-route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const locationsController = require('./locations-controller');
+const { authenticateToken } = require('../../../utils/authenticate-token');
 const route = express.Router();
 
 module.exports = (app) => {
     app.use('/locations', route);
 
-    route.get('/', locationsController.getLocations);
+    route.get('/', authenticateToken, locationsController.getLocations);
 
-    route.get('/:id', locationsController.getLocationById);
+    route.get('/:id', authenticateToken, locationsController.getLocationById);
 };
