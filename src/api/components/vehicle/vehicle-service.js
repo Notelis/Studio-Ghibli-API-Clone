772e51dc16239c5faa6507963276adb91fbd9bd9
WinnyJazz <jewinn2244@gmail.com>
2025-04-24 15:59:52 +0700

    remove extra code

--- src/api/components/vehicle/vehicle-service.js
+++ src/api/components/vehicle/vehicle-service.js
@@ -2,8 +2,6 @@ const vehiclesRepository = require('./vehicle-repository');
 
 async function getVehicles({ filters = {}, offset = 0, limit = 50 }) {
 
-  const { fields } = filters;
-  
   return vehiclesRepository.getVehicles(filters, limit, offset);
 }
 
